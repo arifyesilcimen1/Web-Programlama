2d9dc08e93cb4feac06904793f8a28e327c28742
arifyesilcimen1 <74150655+arifyesilcimen@users.noreply.github.com>
2023-12-27 00:16:55 +0300

    webprogramlama

---
+++ 4HastaneTakipSistemi/HastaneTakipSistemi/HastaneTakipSistemi/Models/HastaYapilanIslemler.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HastaneTakipSistemi.Models
+{
+    public class HastaYapilanIslemler
+    {
+        [Key]
+        public int Id { get; set; }
+
+        
+        
+        public DateTime giris { get; set; }
+
+        public DateTime cikis { get; set; }
+
+        [Display(Name ="Tedavi Açıklaması")]
+        public string tedaviAciklamasi { get; set; }
+
+        [ForeignKey("BarkodOlustur")]
+        public int barkodOlusturId { get; set; }
+        public BarkodOlustur barkodOlustur { get; set; }
+    }
+}
