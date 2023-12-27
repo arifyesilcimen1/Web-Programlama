e7f368995598bb12db10f4d0071ecdd7985427d2
arifyesilcimen1 <74150655+arifyesilcimen@users.noreply.github.com>
2023-12-28 01:28:28 +0300

    web programlama

---
+++ 5HastaneTakipSistemi/HastaneTakipSistemi/HastaneTakipSistemi/Controllers/AcilEkranController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HastaneTakipSistemi.Controllers
+{
+    public class AcilEkranController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
