06cf674cebf2fd9fcb1b1dc06eff890c77d1f50a
arifyesilcimen1 <74150655+arifyesilcimen@users.noreply.github.com>
2023-12-30 00:46:46 +0300

    web programlama

---
+++ HastaneTakipSistemi/HastaneTakipSistemi/Controllers/YapılanIslemGoruntule.cs
@@ -0,0 +1,59 @@
+﻿using HastaneTakipSistemi.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HastaneTakipSistemi.Controllers
+{
+    public class YapılanIslemGoruntule : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public YapılanIslemGoruntule(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: YapılanIslemGoruntule
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Goruntule(string userInput)
+        {
+            /* var barkod= _context.barkodOlusturs.FirstOrDefault(a=> a.tcNo == tc);
+             var gorunutle = _context.hastaYapilanIslemlers.Where(x=> x.barkodOlusturId== barkod.Id).ToList();
+
+
+             return View(gorunutle);*/
+            // Veritabanından gelen tcKimlik'e sahip BarkodOlustur nesnesini bul
+            /*
+             *  var hastaYapilanIslemler = await _context.hastaYapilanIslemlers
+                .Include(h => h.barkodOlustur)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (hastaYapilanIslemler == null)
+            {
+                return NotFound();
+            }
+
+            return View(hastaYapilanIslemler);
+             */
+            var hasta = _context.barkodOlusturs
+                                .Include(b => b.hastaYapilanIslemlers) // İlgili HastaYapilanIslemler'ı getir
+                                .FirstOrDefault(b => b.tcNo == userInput);
+
+
+            // Eğer hasta bulunduysa, hastaYapilanIslemlers listesini kullanabilirsiniz
+            var yapilanIslemler = hasta.hastaYapilanIslemlers.ToList();
+
+            // Diğer işlemleri gerçekleştirin veya view'a gönderin
+            return View(yapilanIslemler);
+
+
+        }
+
+
+    }
+}
---
+++ HastaneTakipSistemi/HastaneTakipSistemi/Program.cs
@@ -0,0 +1,89 @@
+using HastaneTakipSistemi.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = true;
+    options.Password.RequiredLength = 3;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireDigit = false;
+}
+).AddEntityFrameworkStores<ApplicationDbContext>()
+.AddDefaultUI()
+.AddDefaultTokenProviders();
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+    var Roles = new[] { "admin" };
+
+    foreach (var role in Roles)
+    {
+
+        if (!await roleManager.RoleExistsAsync(role))
+            await roleManager.CreateAsync(new IdentityRole(role));
+    }
+}
+using (var scope = app.Services.CreateScope())
+{
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+    string email = "web3@sakarya.edu.tr";
+    string password = "Sau123*";
+    if (await userManager.FindByEmailAsync(email) == null)
+    {
+        var user = new IdentityUser();
+        user.UserName = email;
+        user.Email = email;
+
+        await userManager.CreateAsync(user, password);
+
+
+        await userManager.AddToRoleAsync(user, "admin");
+
+    }
+}
+
+app.Run();
