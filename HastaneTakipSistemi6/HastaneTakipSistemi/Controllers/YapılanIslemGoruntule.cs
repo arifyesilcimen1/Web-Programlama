09b41874b5c2acca6d378bd7abe160fcac5be2e0
arifyesilcimen1 <74150655+arifyesilcimen@users.noreply.github.com>
2023-12-30 16:11:56 +0300

    web-programlama

---
+++ HastaneTakipSistemi6/HastaneTakipSistemi/Controllers/YapılanIslemGoruntule.cs
@@ -0,0 +1,31 @@
+﻿using HastaneTakipSistemi.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HastaneTakipSistemi.Controllers
+{
+    public class YapılanIslemGoruntule : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public YapılanIslemGoruntule(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: YapılanIslemGoruntule
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Goruntule(int barkodId)
+        {
+            var gorunutle = _context.hastaYapilanIslemlers.Where(x => x.barkodOlusturId == barkodId).ToList();
+
+
+            return View(gorunutle);
+        }
+
+       
+    }
+}
