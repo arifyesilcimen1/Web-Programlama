33c58dae307ad9186e79a2b10ad9a6f8ddf800ca
arifyesilcimen1 <74150655+arifyesilcimen@users.noreply.github.com>
2023-12-31 12:02:17 +0300

    web programlama

---
+++ HastaneTakipSistemi7/HastaneTakipSistemi/Controllers/AcilEkranController.cs
@@ -0,0 +1,21 @@
+﻿using HastaneTakipSistemi.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HastaneTakipSistemi.Controllers
+{
+    public class AcilEkranController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AcilEkranController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Ekran()
+        {
+            var liste= _context.barkodOlusturs.ToList();
+
+            return View(liste);
+        }
+    }
+}
---
+++ HastaneTakipSistemi7/HastaneTakipSistemi/Controllers/AdminPanelController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HastaneTakipSistemi.Controllers
+{
+    public class AdminPanelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
